24f25da681968e9da771bca7e50eadcb844a2535
Yvens Rebouças Serpa <y.reboucasserpa@saxion.nl>
2026-02-12 08:08:51 +0100

    First pass on processing ministral and 04-mini

--- Scripts/parse_model_logs.sh
+++ Scripts/parse_model_logs.sh
@@ -19,7 +19,7 @@ mkdir -p "$OUTPUT_FOLDER"
 models=("deepseek-chat" "deepseek-reasoner"
 "claude-haiku-4-5-20251001" "claude-sonnet-4-5-20250929"
 "gemini-2.5-flash-lite" "gemini-2.5-flash"
-"gpt-4.1-mini" "gpt-4.1"
+"gpt-4.1-mini" "gpt-4.1" "o4-mini"
 "magistral-small-2509" "ministral-3b-2512" )
 
 for model in "${models[@]}"; do
